87d715aa4cae44375914ceb3a48a02efd4a63d6c
Alex_04 <165339451+anderh04@users.noreply.github.com>
2024-10-13 22:28:35 -0500

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -4,15 +4,12 @@ $username = "root";
 $password = "";
 $dbname = "php";
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Verificar que se haya enviado una solicitud POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtener los datos del formulario
     $nombre = $_POST['nombre'];
