80c06d121d8127073c75d18bf21c3c1a59d3189d
Alex_04 <165339451+anderh04@users.noreply.github.com>
2024-10-13 22:26:55 -0500

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -4,13 +4,17 @@ $username = "root";
 $password = "";
 $dbname = "php";
 
+// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
+// Verificar que se haya enviado una solicitud POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $cedula = $_POST['cedula'];
@@ -18,23 +22,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $residencia = $_POST['residencia'];
     $email = $_POST['email'];
 
-
     $sql_check = "SELECT * FROM usuarios WHERE email='$email' OR cedula='$cedula'";
     $result = $conn->query($sql_check);
 
     if ($result->num_rows > 0) {
-        echo "<script>alert('El usuario con este email o cédula ya está registrado.');</script>";
+        echo "<script>alert('El usuario con este email o cédula ya está registrado.'); window.location.href = 'index.php';</script>";
     } else {
-
+        
         $sql = "INSERT INTO usuarios (nombre, apellido, cedula, escuela, residencia, email) 
                 VALUES ('$nombre', '$apellido', '$cedula', '$escuela', '$residencia', '$email')";
 
         if ($conn->query($sql) === TRUE) {
-            echo "<script>alert('Usuario registrado con éxito');</script>";
+            
+            echo "<script>alert('Usuario registrado con éxito'); window.location.href = 'index.php';</script>";
         } else {
+            
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }
 }
 
 $conn->close();
+?>
